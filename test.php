2b508cc4130360e551d7be4de342f855ddbd623d
DimaNet <hoxitt@gmail.com>
2019-11-08 00:39:35 +0200

    abc

---
+++ test.php
@@ -0,0 +1,48 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+//include "news/NewsDB.class.php";
+//$homepage = file_get_contents('http://php.net/manual/ru/function.file-get-contents.php');
+//$arr = str_word_count($homepage, 1, 'àáãç3');
+//foreach ($arr as $val){
+//    if($val == "umask"){
+//        echo $val;
+//    }
+//}
+
+//var_dump($homepage);
+
+
+//$text = 'This is a test';
+//echo strlen($text); // 14
+
+//$dom = new DOMDocument();
+//$dom->load("https://lenta.ru/rss");
+//$root = $dom->documentElement;
+//$children = $root->childNodes;
+//print_r($children);
+
+
+
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<p></p>
+</body>
+</html>
+
+
+
+
+
+
